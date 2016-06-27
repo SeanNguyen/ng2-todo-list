3bcda516ec3e04726ca9d26ad01a2ff3bdde9bad
Nguyen, Sean <tronguyen@paypal.com>
2016-06-27 20:49:04 +0800

    compress response data

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var app = express();
+var express = require("express");
+var compression = require("compression");
 
+var app = express();
+app.use(compression());
 app.use('/', express.static('.'));
 
 app.listen(3000, function () {
