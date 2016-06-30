5ec50d81747d18366a48377fe65bfb2e4a70ff14
Nguyen, Sean <tronguyen@paypal.com>
2016-06-30 12:07:52 +0800

    fix route link

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,12 @@
  * Created by seann on 26/6/2016.
  */
 
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit, OnDestroy} from "@angular/core";
 import { TodoItemService } from "./todoItem.service";
 import { Todo } from "./todo.model";
 import {TodoComponent} from "./todo/component";
 import _ = require("lodash");
-import {ROUTER_DIRECTIVES, ActivatedRoute} from "@angular/router";
+import {ROUTER_DIRECTIVES, ActivatedRoute, Router, NavigationEnd} from "@angular/router";
 import {Subscription} from "rxjs/Rx";
 
 @Component({
@@ -16,17 +16,25 @@ import {Subscription} from "rxjs/Rx";
     templateUrl: "app/app.html",
     directives: [TodoComponent, ROUTER_DIRECTIVES]
 })
-export class TodoApp implements OnInit {
+export class TodoApp implements OnInit, OnDestroy {
     public newTodo: Todo = new Todo();
     public activatedPath = '';
-    private activatedPathSub: Subscription;
+    private subscriptions: Subscription[] = [];
 
-    constructor(public todoService: TodoItemService, private route: ActivatedRoute) {}
+    constructor(public todoService: TodoItemService, private route: ActivatedRoute, private router: Router) {}
 
     public ngOnInit() {
-        this.activatedPathSub = this.route.url.subscribe(urlPathWithParams => {
-            console.log(urlPathWithParams);
-        });
+        this.subscriptions.push(this.router.events.subscribe(event => {
+            if(event instanceof NavigationEnd){
+                this.activatedPath = event.url;
+            }
+        }));
+    }
+
+    public ngOnDestroy() {
+        _.each(this.subscriptions, (subscription: Subscription) => {
+            subscription.unsubscribe();
+        })
     }
 
     public addTodoItem() {
